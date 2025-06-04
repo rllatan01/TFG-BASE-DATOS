47cd0ab1c31536ae9d6b08fae92ba147f9711a71
rllatan01 <rllatan01@educantabria.es>
2025-06-04 10:11:09 +0200

    .

--- script_tls_haproxy.sh
+++ script_tls_haproxy.sh
@@ -1,55 +1,56 @@
-    #!/bin/bash
+#!/bin/bash
 
-    DOMAIN="wordpress-rufino.duckdns.org"
-    EMAIL="rllatan01@educantabria.es"
-    APACHE_IPS=("10.215.20.100" "10.215.20.101")
+DOMAIN="wordpress-rufino.duckdns.org"
+EMAIL="rllatan01@educantabria.es"
+APACHE_IPS=("10.215.20.100" "10.215.20.101")
 
-    apt update
-    apt install -y certbot openssh-client
-    systemctl stop haproxy
-    # Obtener certificado
-    certbot certonly --standalone --non-interactive --agree-tos --email $EMAIL -d $DOMAIN
-    # Combinar .pem
-    mkdir -p /etc/haproxy/certs
-    cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem > /etc/haproxy/certs/$DOMAIN.pem
-    chmod 600 /etc/haproxy/certs/$DOMAIN.pem
-    rm -r /etc/haproxy/haproxy.cfg
-    # Configurar HAProxy
-    cat << 'EOF' > /etc/haproxy/haproxy.cfg
-    global
-        log /dev/log local0
-        log /dev/log local1 notice
-        daemon
-        maxconn 2048
+apt update
+apt install -y certbot openssh-client
+systemctl stop haproxy
+# Obtener certificado
+certbot certonly --standalone --non-interactive --agree-tos --email $EMAIL -d $DOMAIN
+# Combinar .pem
+mkdir -p /etc/haproxy/certs
+cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem > /etc/haproxy/certs/$DOMAIN.pem
+chmod 600 /etc/haproxy/certs/$DOMAIN.pem
+rm -r /etc/haproxy/haproxy.cfg
+# Configurar HAProxy
+cat << 'EOF' > /etc/haproxy/haproxy.cfg
+global
+    log /dev/log local0
+    log /dev/log local1 notice
+    daemon
+    maxconn 2048
 
-    defaults
-        log global
-        mode http
-        option httplog
-        timeout connect 5s
-        timeout client 50s
-        timeout server 50s
+defaults
+    log global
+    mode http
+    option httplog
+    timeout connect 5s
+    timeout client 50s
+    timeout server 50s
 
-    frontend https_front
-        bind *:443 ssl crt /etc/haproxy/certs/$DOMAIN.pem
-        default_backend wordpress_backend
+frontend https_front
+    bind *:443 ssl crt /etc/haproxy/certs/$DOMAIN.pem
+    default_backend wordpress_backend
 
-    frontend http_front
-        bind *:8080
-        redirect scheme https if !{ ssl_fc }
+frontend http_front
+    bind *:8080
+    redirect scheme https if !{ ssl_fc }
 
-    backend wordpress_backend
-        balance roundrobin
-        server web1 10.215.20.100:443 ssl verify none check
-        server web2 10.215.20.101:443 ssl verify none check
-    EOF
+backend wordpress_backend
+    balance roundrobin
+    server web1 10.215.20.100:443 ssl verify none check
+    server web2 10.215.20.101:443 ssl verify none check
     
-    systemctl enable haproxy
-    systemctl restart haproxy
+EOF
+    
+systemctl enable haproxy
+systemctl restart haproxy
 
-    # Copiar certificados a los servidores Apache usando la clave generada
-    for ip in "${APACHE_IPS[@]}"; do
-      echo "Copiando certificados a $ip..."
-      scp -i /home/ubuntu/vockey.pem /etc/letsencrypt/live/$DOMAIN/fullchain.pem ubuntu@$ip:/tmp/
-      scp -i /home/ubuntu/vockey.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem ubuntu@$ip:/tmp/
-    done
+# Copiar certificados a los servidores Apache usando la clave generada
+for ip in "${APACHE_IPS[@]}"; do
+  echo "Copiando certificados a $ip..."
+  scp -i /home/ubuntu/vockey.pem /etc/letsencrypt/live/$DOMAIN/fullchain.pem ubuntu@$ip:/tmp/
+  scp -i /home/ubuntu/vockey.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem ubuntu@$ip:/tmp/
+done
--- script_tls_wordpress.sh
+++ script_tls_wordpress.sh
@@ -1,32 +1,30 @@
-UserData:
-  Fn::Base64: !Sub |
-    #!/bin/bash
-    DOMAIN="wordpress-rufino.duckdns.org"
-    SSL_DIR="/etc/ssl/live/$DOMAIN"
+#!/bin/bash
+DOMAIN="wordpress-rufino.duckdns.org"
+SSL_DIR="/etc/ssl/live/$DOMAIN"
 
-    mkdir -p $SSL_DIR
-    mv /tmp/fullchain.pem $SSL_DIR/
-    mv /tmp/privkey.pem $SSL_DIR/
-    chmod 600 $SSL_DIR/privkey.pem
-    chmod 644 $SSL_DIR/fullchain.pem
+mkdir -p $SSL_DIR
+mv /tmp/fullchain.pem $SSL_DIR/
+mv /tmp/privkey.pem $SSL_DIR/
+chmod 600 $SSL_DIR/privkey.pem
+chmod 644 $SSL_DIR/fullchain.pem
 
-    a2enmod ssl
+a2enmod ssl
 
-    cat > /etc/apache2/sites-available/$DOMAIN-ssl.conf <<EOF
-    <VirtualHost *:443>
-        ServerName $DOMAIN
-        DocumentRoot /var/www/html
+cat > /etc/apache2/sites-available/$DOMAIN-ssl.conf <<EOF
+<VirtualHost *:443>
+    ServerName $DOMAIN
+    DocumentRoot /var/www/html
 
-        SSLEngine on
-        SSLCertificateFile $SSL_DIR/fullchain.pem
-        SSLCertificateKeyFile $SSL_DIR/privkey.pem
+    SSLEngine on
+    SSLCertificateFile $SSL_DIR/fullchain.pem
+    SSLCertificateKeyFile $SSL_DIR/privkey.pem
 
-        <Directory /var/www/html>
-            AllowOverride All
-        </Directory>
-    </VirtualHost>
-    EOF
+    <Directory /var/www/html>
+        AllowOverride All
+    </Directory>
+</VirtualHost>
+EOF
 
-    a2ensite $DOMAIN-ssl
-    a2dissite 000-default.conf
-    systemctl reload apache2
+a2ensite $DOMAIN-ssl
+a2dissite 000-default.conf
+systemctl reload apache2
